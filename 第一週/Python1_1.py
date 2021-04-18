8cfc4ffeebdde5a5756591cd5337487b75941c93
d22566813 <d22566813@gmail.com>
2021-04-18 13:32:54 +0800

    [Added]第二週練習:v1

--- 第一週/Python1_1.py
+++ 第一週/Python1_1.py
@@ -1,22 +1,20 @@
-list=[['A','B','C'], ['A','C','B'], ['B','A','C']]
-number=input("How many plates?")
+list = [['A', 'B', 'C'], ['A', 'C', 'B'], ['B', 'A', 'C']]
+number = input("How many plates?")
 
 while number.isdigit():
-    if int(number)==1:
+    if int(number) == 1:
         list[0]
         print(list[0]+"number=1, move from A to C")
         exit(0)
-    if int(number)!=1:
-        i=2
-        while i<int(number):
-            list=list[i]
-            ifi==2:
+    if int(number) != 1:
+        i = 2
+        while i < int(number):
+            list = list[i]
+            if i == 2:
                 print(list[i]+"number=number-1, move from B to C")
-            if i==1:
+            if i == 1:
                 print(list[i]+"number=number-1, move from A to B")
-            i-=1           
-                  
-            if int(number)<=0:
-                exit(0)
+            i -= 1
 
-    
+            if int(number) <= 0:
+                exit(0)
