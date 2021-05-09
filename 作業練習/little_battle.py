e1b0d993bd0a732ff322d749ce65759325d3dc27
d22566813 <d22566813@gmail.com>
2021-05-10 00:51:05 +0800

    [Added]作業:v3

--- 作業練習/little_battle.py
+++ 作業練習/little_battle.py
@@ -3,8 +3,7 @@ import sys
 # Please implement this function according to Section "Read Configuration File"
 
 
-class battle_map:
-  # 建構式
+class BattleMap:
     def __init__(self, width, height, waters, woods, foods, golds, h1, h2):
         self.width = width
         self.height = height
@@ -34,19 +33,24 @@ class battle_map:
 
     def draw_map(self):
         print("Please check the battlefield, commander.")
+        #-------------x-content && y-content-------------#
+        # region
         w = 0
-        w_s = ""
+        w_xs = ""
         w_ys = ""
         while w < self.width:
             if w == 0:
-                w_s = w_s+"0"+str(w)
+                w_xs = w_xs+"0"+str(w)
                 w_ys = w_ys+"--"
             else:
-                w_s = w_s+" 0"+str(w)
+                w_xs = w_xs+" 0"+str(w)
                 w_ys = w_ys+"---"
             w = w+1
-        w_s = "  X"+w_s+"X"
+        w_xs = "  X"+w_xs+"X"
         w_ys = " Y+"+w_ys+"+"
+        # endregion
+        #-------------map-content-------------#
+        # region
         y = 0
         h_s = ""
         while y < self.height:
@@ -55,14 +59,18 @@ class battle_map:
             while x < self.width:
                 h_s = h_s+"|"
                 empty = True
+                # player 1 Base
                 if self.h1.x == x and self.h1.y == y:
                     h_s = h_s+"H1"
                     empty = False
+                # player 2 Base
                 elif self.h2.x == x and self.h2.y == y:
                     h_s = h_s+"H2"
                     empty = False
+                # assets Icon
                 for icon in ['~~', 'WW', 'FF', 'GG']:
                     h_s, empty = self.draw_map_assets(icon, h_s, x, y, empty)
+                # None Icon
                 if empty:
                     h_s = h_s+"  "
                 if x == (self.width-1):
@@ -71,21 +79,21 @@ class battle_map:
             if y != (self.height-1):
                 h_s = h_s+"\n"
             y += 1
-        print(w_s)
+        # endregion
+        #-------------draw-map-------------#
+        print(w_xs)
         print(w_ys)
         print(h_s)
         print(w_ys)
 
 
 class Position:
-  # 建構式
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
 class Player:
-  # 建構式
     def __init__(self, name, wood, food, gold, spearman, archer, knight, scout, home):
         self.name = name
         self.wood = wood
@@ -116,46 +124,44 @@ def load_config_file(filepath):
     # Complete the test driver of this function in file_loading_test.py
     width, height = 0, 0
     waters, woods, foods, golds = [], [], [], []  # list of position tuples
-    f = open(filepath, 'r')
-    content = f.read()
-    for word in content.splitlines(keepends=False):
-        # word = word.replace("Frame: ", "")
-        # print(word)
-        if "Frame: " in word:
-            word = word.replace("Frame: ", "")
-            map_wh = word.split("x")
-            width = int(map_wh[0])
-            height = int(map_wh[1])
-            # print(word)
-        elif "Water: " in word:
-            word = word.replace("Water: ", "")
-            assets = word.split(" ")
-            # print("Water: ")
-            # print(assets)
-            # print("-------")
-            waters = set_assets(assets)
-        elif "Wood: " in word:
-            word = word.replace("Wood: ", "")
-            assets = word.split(" ")
-            woods = set_assets(assets)
-        elif "Food: " in word:
-            word = word.replace("Food: ", "")
-            assets = word.split(" ")
-            foods = set_assets(assets)
-        elif "Gold: " in word:
-            word = word.replace("Gold: ", "")
-            assets = word.split(" ")
-            golds = set_assets(assets)
-
+    try:
+        f = open(filepath, 'r')
+        content = f.read()
+        for word in content.splitlines(keepends=False):
+            if "Frame: " in word:
+                word = word.replace("Frame: ", "")
+                map_wh = word.split("x")
+                width = int(map_wh[0])
+                height = int(map_wh[1])
+            elif "Water: " in word:
+                word = word.replace("Water: ", "")
+                assets = word.split(" ")
+                waters = set_assets(assets)
+            elif "Wood: " in word:
+                word = word.replace("Wood: ", "")
+                assets = word.split(" ")
+                woods = set_assets(assets)
+            elif "Food: " in word:
+                word = word.replace("Food: ", "")
+                assets = word.split(" ")
+                foods = set_assets(assets)
+            elif "Gold: " in word:
+                word = word.replace("Gold: ", "")
+                assets = word.split(" ")
+                golds = set_assets(assets)
+    except IOError:
+        print("FileNotFoundError")
+    finally:
+        f.close()
     return width, height, waters, woods, foods, golds
 
 
 def show_recruit_price():
     print("Recruit Prices:")
-    print("Spearman (S) -1W, 1F")
-    print("Archer (A) -1W, 1G")
-    print("Knight (K) -1F, 1G")
-    print("Scout (T) -1W, 1F, 1G")
+    print("  Spearman (S) -1W, 1F")
+    print("  Archer (A) -1W, 1G")
+    print("  Knight (K) -1F, 1G")
+    print("  Scout (T) -1W, 1F, 1G")
 
 
 def show_year(year):
@@ -169,11 +175,13 @@ def show_asstes(palyer):
 
 def show_recruit_msg(recruit_type):
     input_content = input(
-        "You want to recruit a {}.Enter two integers as format 'x y' to place your army.", recruit_type)
+        "You want to recruit a {}.Enter two integers as format 'x y' to place your army.".format(
+            recruit_type)
+    )
     input_xy = input_content.splt(' ')
 
 
-def check_home_base_empy(armies, check_p, check_p_list):
+def check_home_place_empty(armies, check_p, check_p_list):
     for p in armies:
         if (p.x == check_p[0].x and p.y == check_p[0].y) and check_p_list[0]:
             check_p_list[0] = False
@@ -189,33 +197,47 @@ def check_home_base_empy(armies, check_p, check_p_list):
 def recruit_stage(player, bt_map):
     print("+++"+player.name+"'s Stage: Recruit Armies+++")
     show_asstes(player)
-    input_content = ""
+    #-------------check resource is enough-------------#
+    # region
     if (player.wood == 0 and player.food == 0) or (player.wood == 0 and player.gold == 0) or (player.gold == 0 and player.food == 0):
         print('No resources to recruit any armies.')
         return
+    # endregion
 
-    check_p = [Position(player.home.x-1, player.home.y),
-               Position(player.home.x+1, player.home.y),
-               Position(player.home.x, player.home.y+1),
-               Position(player.home.x, player.home.y-1)
-               ]
-    check_p_list = [True, True, True, True]
+    #-------------check has place to recruit-------------#
+    # region
+    check_home_p = [
+        Position(player.home.x-1, player.home.y),
+        Position(player.home.x+1, player.home.y),
+        Position(player.home.x, player.home.y+1),
+        Position(player.home.x, player.home.y-1)
+    ]
 
-    check_p_list = check_home_base_empy(player.spearman, check_p, check_p_list)
-    check_p_list = check_home_base_empy(player.archer, check_p, check_p_list)
-    check_p_list = check_home_base_empy(player.knight, check_p, check_p_list)
-    check_p_list = check_home_base_empy(player.scout, check_p, check_p_list)
+    check_p_empty_list = [True, True, True, True]
 
-    check = True
+    check_p_empty_list = check_home_place_empty(
+        player.spearman, check_home_p, check_p_empty_list)
+    check_p_empty_list = check_home_place_empty(
+        player.archer, check_home_p, check_p_empty_list)
+    check_p_empty_list = check_home_place_empty(
+        player.knight, check_home_p, check_p_empty_list)
+    check_p_empty_list = check_home_place_empty(
+        player.scout, check_home_p, check_p_empty_list)
 
-    for c in check_p_list:
+    check_place = True
+
+    for c in check_p_empty_list:
         if c:
-            check = False
+            check_place = False
 
-    if check:
+    if check_place:
         print('No place to recruit any armies.')
         return
+    # endregion
 
+    #----------------recruit army-----------------------#
+    # region
+    input_content = ""
     while input_content != 'NO':
         input_content = input(
             "Which type of army to recruit, (enter) 'S', 'A', 'K', or 'T'? Ener 'NO' to end this stage.\n")
@@ -225,6 +247,8 @@ def recruit_stage(player, bt_map):
             show_recruit_price()
         elif input_content == 'QUIT':
             exit()
+        elif input_content == 'NO':
+            break
         elif input_content == 'S':
             show_recruit_msg("Spearman")
             show_asstes(player)
@@ -239,6 +263,7 @@ def recruit_stage(player, bt_map):
             show_asstes(player)
         else:
             print("Sorry, invalid input Try again.")
+    # endregion
 
 
 def move_stage():
@@ -251,39 +276,46 @@ if __name__ == "__main__":
         sys.exit()
     # -------- load config.txt -----------#
     width, height, waters, woods, foods, golds = load_config_file(sys.argv[1])
+    print("Configuration file config.txt was loaded.")
     # -----------------------------------#
 
     # ----------Player initial-----------#
-    player1 = Player("Player 1", 2, 2, 2, [], [], [], [], Position(1, 1))
-    player2 = Player("Player 2", 2, 2, 2, [], [], [],
-                     [], Position(width-2, height-2))
+    player1 = Player("Player 1", 2, 2, 2,
+                     [], [], [], [], Position(1, 1))
+    player2 = Player("Player 2", 2, 2, 2,
+                     [], [], [], [], Position(width-2, height-2))
     # -----------------------------------#
 
     # ----------Map initial-----------#
-    game_map = battle_map(width, height, waters, woods,
-                          foods, golds, player1.home, player2.home)
+    game_map = BattleMap(width, height, waters, woods,
+                         foods, golds, player1.home, player2.home)
     # -----------------------------------#
 
     # ----------Year initial-----------#
     year = 617
     # -----------------------------------#
 
-    # ----------Game Start-----------#
-    print("Configuration file config.txt was loaded.")
+    # ----------<*Game Start*>-----------#
     print("Game Started: Little Battle! (enter QUIT to quit the game)\n")
 
     # ----------Draw Map-----------#
     game_map.draw_map()
-
     print("(enter DIS to display the map)\n\n")
+    # -----------------------------------#
+
+    # ----------Show recruit price-----------#
     show_recruit_price()
     print("(enter PRIS to display the price list)\n\n")
+    # -----------------------------------#
+
     # ----------Show Year-----------#
     show_year(year)
+    # -----------------------------------#
 
     # ----------Stage_Recruit-----------#
     recruit_stage(player1, game_map)
     # -----------------------------------#
-    # ----------Stage_MOve-----------#
+
+    # ----------Stage_Move-----------#
     # move_stage(player1, game_map)
     # -----------------------------------#
